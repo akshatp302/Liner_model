19b06cc53ef76c5004914fbe004a1b7d18d31a52
akshatp302 <akshatpanchal779@gmail.com>
2025-03-18 19:32:45 +0100

    new update

--- linear_model.py
+++ linear_model.py
@@ -3,18 +3,9 @@ import matplotlib.pyplot as plt
 import torch
 from torch import nn
 
-class Liner_model(nn.Module):
-    
-    def __init__(self):
-        super().__init__()
-        
-        self.linear_layer = nn.Linear(in_features=1,
-                                      out_features=1)
-        
-    def forward (self,x):
-         
-        return self.linear_layer(x)
-        
+class load_visulization():
+    def __init__(self,input_step):
+        self.input_step = input_step
             
     def data_loader(self):
         start = 0
@@ -23,18 +14,38 @@ class Liner_model(nn.Module):
         label_bias = 0.3
         
         
-        x = torch.arange(start=start,end=end,step=self.input_data)
-        y = label_weight*self.input_data + label_bias
+        self.x = torch.arange(start=start,end=end,step=self.input_step).unsqueeze(dim=1)
+        self.y = label_weight*self.x + label_bias
         
-        split  = int(len(x)*0.80)
+        split  = int(len(self.x)*0.80)
         
-        x_train_split = x[:split]
-        y_train_split = y[:split]
+        self.x_train_split = self.x[:split]
+        self.y_train_split = self.y[:split]
         
-        x_test_split = x[split:]
-        y_test_split = y[split:]
+        self.x_test_split = self.x[split:]
+        self.y_test_split = self.y[split:]
         
-
+    def visulization(self,prediction):
         
-        
-        
+       plt.figure(figsize=(10,7),dpi=150)
+       
+       plt.scatter(x=self.x_train_split,
+                   y=self.y_train_split,
+                   c="blacl",s=3,label="train-set")
+       
+       plt.scatter(x=self.x_test_split,
+                   y=self.y_test_split,
+                   c="blue",s=4,label="test-set")
+       
+       if prediction is not None:
+       
+            plt.scatter(x=self.x_test_split,
+                        y=prediction,
+                        c="green",s=2,label="model-output")
+            
+            
+    plt.xlabel("data")
+    plt.ylabel("label")
+    plt.title("model_data")
+    plt.legend()
+    plt.show()
